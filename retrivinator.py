25cdd3a7aef30e0032f4663f82b9e29ed7e85002
TongueDaddy <z.mitarotonda@gmail.com>
2020-08-13 15:27:17 -0400

    palying with the keyboard controls

--- retrivinator.py
+++ retrivinator.py
@@ -1,45 +1,77 @@
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 from secrets import CLIENT_ID, CLIENT_SECRETS, USERNAME
 import json
-
+import os
+import keyboard
 from timemachininator import convert
-
-scope = 'user-read-playback-state user-modify-playback-state'
-token = spotipy.util.prompt_for_user_token(USERNAME, scope, client_id=CLIENT_ID,
+#fucntion tyme
+def Volume():
+    sp.next_track()
+def prevTrack():
+    sp.previous_track()
+def skipTrack():
+    sp.next_track()
+def pausePlayback():
+    sp.pause_playback()
+def startPlayback():
+    sp.start_playback()
+def getToken():
+    scope = 'user-read-playback-state user-modify-playback-state'
+    return spotipy.util.prompt_for_user_token(USERNAME, scope, client_id=CLIENT_ID,
                                         client_secret=CLIENT_SECRETS, redirect_uri='https://github.com/TongueDaddy')
+def getInfo():
+            currentTrack = current['item']
+            currentArtist = current['item']['artists'][0]
+            currentAlbum = current ['item']['album']
+            progressTime = (round(current['progress_ms']/1000))
+            durationTime = (round(current['item']['duration_ms']/1000))
+            albumCover = current ['item']['album']['images'][0]
+            volume = current['device']['volume_percent']
+            isShuffle = current['shuffle_state']
+            isRepeat = current['repeat_state']
+            print('Zack is now playing' + ' ' + currentTrack['name'])#track
+            print('by'+ ' ' + currentArtist['name'])#artist
+            print('on'+ ' ' + currentAlbum['name'])#album
+            print(convert(progressTime) + ' ' + '---' + ' ' + convert(durationTime))#time
+            print(albumCover['url'])#picutre url
+            print('at' + ' ' + str(volume))#how much luoud
+            print('shuffle=' + str(isShuffle) + ' ' + 'repeat=' + str(isRepeat))# shuffle and repeat state
+if __name__ == "__main__":
+    token = getToken()
+    # sex time
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        results = sp.currently_playing()
+        #add shuffle state, volume %, repeat state*
+        current = sp.current_playback()
 
-# sex time
-if token:
-    sp = spotipy.Spotify(auth=token)
-    #results = sp.currently_playing()
-    #add shuffle state, volume %, repeat state
-    current = sp.current_playback()
-    isPlaying = current['is_playing']
-
-    if isPlaying is not True:
-        input('hey turn on the music idiothead')
+        if current is None or current['is_playing'] is not True:
+            print('hey we dont exist :O')
+            input('say something when you are playing otherwise ima just chill: ')
+            sp.start_playback()
+            
+            # please start playing music when i want it to
+            # then look for what song is playing
         
-    if current is None:
-        print('hey we dont exist :O')
-        input('say something when you are playing otherwise ima just chill: ') 
+        with open('jason.json', 'w') as outfile:
+            json.dump(current, outfile)
 
-    with open('jason.json', 'w') as outfile:
-        json.dump(current, outfile)
-
-    currentTrack = current['item']
-    currentArtist = current['item']['artists'][0]
-    currentAlbum = current ['item']['album']
-    progressTime = (round(current['progress_ms']/1000))
-    durationTime = (round(current['item']['duration_ms']/1000))
-    albumCover = current ['item']['album']['images'][0]
-    volume = current['device']['volume_percent']
-    isShuffle = current['shuffle_state']
-    isRepeat = current['repeat_state']
-    print('Zack is now playing' + ' ' + currentTrack['name'])
-    print('by'+ ' ' + currentArtist['name'])
-    print('on'+ ' ' + currentAlbum['name'])
-    print(convert(progressTime) + ' ' + '---' + ' ' + convert(durationTime))
-    print(albumCover['url'])
-    print('at' + ' ' + str(volume))
-    print('shuffle=' + str(isShuffle) + ' ' + 'repeat=' + str(isRepeat))
+        getInfo()#this is info going to display
+        while True:
+            try:
+                if keyboard.is_pressed('right arrow'): #keyboard controls
+                    sp.next_track()
+                    pass
+                if keyboard.is_pressed('left arrow'):
+                    sp.previous_track()
+                    pass
+                if keyboard.is_pressed('space'):
+                    try:
+                        sp.start_playback()
+                        pass
+                    except:
+                        sp.pause_playback()
+                        pass
+            except:
+                print('uh oh, theres an oopsies')
+                break
